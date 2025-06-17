6ba73dda597aaa4ffb3b2c0fcdb5cf613999b1e1
Tayansam <samiraramo815@gmail.com>
2025-06-17 20:44:22 +0300

    updated form.js and style.css

--- form.js
+++ form.js
@@ -1,20 +1,22 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxmGKV9LMPNcgc_sEM7NXJAkWFidAoUygspRqH05b38X78jMo4LIrf2VJdSFwsHVFcG/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxmGKV9LMPNcgc_sEM7NXJAkWFidAoUygspRqH05b38X78jMo4LIrf2VJdSFwsHVFcG/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-
-  msg.innerHTML = "Sending message..."
-  setTimeout(() => { msg.innerHTML = "Message sent succesfully!" }, 3000)
-  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then(response => {
-      form.reset()
+  msg.innerHTML = "Sending message...";
+  setTimeout(() => {
+    msg.innerHTML = "Message sent succesfully!";
+  }, 3000);
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      form.reset();
       setTimeout(() => {
-        msg.innerHTML = ""
-      }, 2000)
-      form.reset()
+        msg.innerHTML = "";
+      }, 2000);
+      form.reset();
     })
-    .catch(error => console.error('Error!', error.message))
-})
+    .catch((error) => console.error("Error!", error.message));
+});
