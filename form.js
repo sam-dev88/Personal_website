9917193a16c5eb13b284a4254ce2d796b76ae7f6
Tayansam <samiraramo815@gmail.com>
2025-06-17 20:35:54 +0300

    updated form.js, index.html and style.css

---
+++ form.js
@@ -0,0 +1,20 @@
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxmGKV9LMPNcgc_sEM7NXJAkWFidAoUygspRqH05b38X78jMo4LIrf2VJdSFwsHVFcG/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  msg.innerHTML = "Sending message..."
+  setTimeout(() => { msg.innerHTML = "Message sent succesfully!" }, 3000)
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+      form.reset()
+      setTimeout(() => {
+        msg.innerHTML = ""
+      }, 2000)
+      form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
